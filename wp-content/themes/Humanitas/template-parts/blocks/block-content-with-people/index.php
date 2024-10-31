c654b73522a1612a43c6585c4ad0c99774f02dfc
Krzysztof Leśniewski <krzysztof.lesniewski@boldare.com>
2024-10-31 13:10:29 +0100

    fix: elements style update

--- wp-content/themes/Humanitas/template-parts/blocks/block-content-with-people/index.php
+++ wp-content/themes/Humanitas/template-parts/blocks/block-content-with-people/index.php
@@ -7,45 +7,58 @@ $people = get_field( 'people' );
 $block_ID = $block['id'];
 $block_style = $block_style ?? get_field('block_style') ?? 'with-contact-person';
 
-?>
+$is_hidden = get_field('is_hidden');
+$sub_title = get_field('sub_title');
+$description = get_field('description');
 
-<section class="block-content-with-people block-content-with-people--<?= $block_style;?>" id="<?=$block_ID; ?>">
+?>
+<?php if (!$is_hidden) : ?>
+  <section class="block-content-with-people block-content-with-people--<?= $block_style;?>" id="<?=$block_ID; ?>">
     <div class="container">
+      <?php if ($title) : ?>
         <h2 class="block-content-with-people__title heading-underline heading-dot fade-in"><?= $title; ?></h2>
-        <?php if($content) :?>
-            <div class="block-content-with-people__content fade-in">
-                <?= $content; ?>
-            </div>
-        <?php endif; ?>
-        <div class="block-content-with-people__wrapper">
-            <?php if($contact_person && $block_style === 'with-contact-person') : ?>
-                <div class="block-content-with-people__contact-person">
-                    <?php
-                        $image = get_post_thumbnail_id($contact_person);
-                        $title = get_the_title($contact_person);
-                        $text = get_field('short_description', $contact_person);
-                        $link = ['url' => '#', 'title' => 'Skontaktuj się'];
-                        ?>
-                    <?php get_theme_part('elements/contact-box', [
-                        'contact_box' => [
-                            'image' => $image,
-                            'subtitle' => __('Twój opiekun', 'humanitas'),
-                            'title' => $title,
-                            'text' => $text,
-                            'link' => $link,
-                        ],
-                        'version' => 'secondary'
-                    ]); ?>
-                </div>
-            <?php endif; ?>
-            <div class="block-content-with-people__people">
-                <?php foreach($people as $person) : ?>
-                    <?php get_theme_part('elements/person-card', [
-                        'post_ID' => $person,
-                        'version' => $block_style === 'big-people' ? 'big' : 'primary'
-                    ]); ?>
-                <?php endforeach; ?>
-            </div>
+      <?php endif; ?>
+      <?php if ($sub_title) : ?>
+        <h3 class="block-content-with-people__sub-title"><?php echo $sub_title; ?></h3>
+      <?php endif; ?>
+      <?php if ($description) : ?>
+        <h4 class="block-content-with-people__description"><?php echo $description; ?></h4>
+      <?php endif; ?>
+      <?php if($content) :?>
+        <div class="block-content-with-people__content fade-in">
+          <?= $content; ?>
         </div>
+      <?php endif; ?>
+      <div class="block-content-with-people__wrapper">
+        <?php if($contact_person && $block_style === 'with-contact-person') : ?>
+          <div class="block-content-with-people__contact-person">
+            <?php
+            $image = get_post_thumbnail_id($contact_person);
+            $title = get_the_title($contact_person);
+            $text = get_field('short_description', $contact_person);
+            $link = ['url' => '#', 'title' => 'Skontaktuj się'];
+            ?>
+            <?php get_theme_part('elements/contact-box', [
+              'contact_box' => [
+                'image' => $image,
+                'subtitle' => __('Twój opiekun', 'humanitas'),
+                'title' => $title,
+                'text' => $text,
+                'link' => $link,
+              ],
+              'version' => 'secondary'
+            ]); ?>
+          </div>
+        <?php endif; ?>
+        <div class="block-content-with-people__people">
+          <?php foreach($people as $person) : ?>
+            <?php get_theme_part('elements/person-card', [
+              'post_ID' => $person,
+              'version' => $block_style === 'big-people' ? 'big' : 'primary'
+            ]); ?>
+          <?php endforeach; ?>
+        </div>
+      </div>
     </div>
-</section>
+  </section>
+<?php endif; ?>
--- wp-content/themes/Humanitas/template-parts/elements/person-card.php
+++ wp-content/themes/Humanitas/template-parts/elements/person-card.php
@@ -2,13 +2,17 @@
 
 $post_ID = $post_ID ?? get_the_ID();
 $position = get_field('position', $post_ID);
+$position_description = get_field('position_description', $post_ID);
 $name = get_the_title($post_ID);
 // post thumbnail id
 $thumbnail = get_post_thumbnail_id($post_ID);
 $phone_number = get_field('phone_number', $post_ID);
 $email_address = get_field('email_address', $post_ID);
+$additional_email_address = get_field('additional_email_address', $post_ID);
+$additional_phone_number = get_field('additional_phone_number', $post_ID);
 $class = $class ?? '';
 $version = $version ?? 'primary';
+
 ?>
 <div class="person-card person-card--<?= $version; ?> <?= $class; ?>">
     <figure class="person-card__image">
@@ -19,7 +23,10 @@ $version = $version ?? 'primary';
         <?php endif; ?>
     </figure>
     <div class="person-card__content">
-        <p class="person-card__position"><?= $position; ?></p>
+        <div class="person-card__position"><?= $position; ?></div>
+		<?php if ($position_description) : ?>
+			<div class="person-card__position-description"><?= $position_description; ?></div>
+		<?php endif; ?>
         <h3 class="person-card__name"><?= $name; ?></h3>
         <?php if($version === 'primary') : ?>
             <div class="person-card__contact">
@@ -31,6 +38,20 @@ $version = $version ?? 'primary';
                         <span class="person-card__contact-text"><?= $phone_number; ?></span>
                     </a>
                 <?php endif; ?>
+
+				<?php if( $additional_phone_number ): ?>
+			        <?php foreach ( $additional_phone_number as $phone_number ) : ?>
+                        <?php if (!empty($phone_number['phone_number'])) : ?>
+                            <a href="tel:<?= $phone_number; ?>" class="person-card__contact-item">
+                                <span class="person-card__contact-icon">
+                                    <?= get_image('phone'); ?>
+                                </span>
+                                <span class="person-card__contact-text"><?= $phone_number['phone_number']; ?></span>
+                            </a>
+                        <?php endif; ?>
+			        <?php endforeach; ?>
+		        <?php endif; ?>
+				
                 <?php if ($email_address) : ?>
                     <a href="mailto:<?= $email_address; ?>" class="person-card__contact-item">
                         <span class="person-card__contact-icon">
@@ -39,6 +60,18 @@ $version = $version ?? 'primary';
                         <span class="person-card__contact-text"><?= $email_address; ?></span>
                     </a>
                 <?php endif; ?>
+                <?php if( $additional_email_address ): ?>
+			        <?php foreach ( $additional_email_address as $email ) : ?>
+                        <?php if (!empty($email['single_mail'])) : ?>
+                            <a href="mailto:<?= esc_attr($email['single_mail']); ?>" class="person-card__contact-item">
+                                <span class="person-card__contact-icon">
+                                    <?= get_image('email'); ?>
+                                </span>
+                                <?= esc_html($email['single_mail']); ?>
+                            </a>
+                        <?php endif; ?>
+			        <?php endforeach; ?>
+		        <?php endif; ?>
             </div>
         <?php endif; ?>
     </div>
