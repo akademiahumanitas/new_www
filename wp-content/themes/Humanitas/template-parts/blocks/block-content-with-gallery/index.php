0d74f779348db77a59ef14f2da426a342de2c843
Krzysztof Leśniewski <krzysztof.lesniewski@boldare.com>
2024-11-12 17:23:30 +0100

    fix: block with gallery. New shape of data input

--- wp-content/themes/Humanitas/template-parts/blocks/block-content-with-gallery/index.php
+++ wp-content/themes/Humanitas/template-parts/blocks/block-content-with-gallery/index.php
@@ -1,37 +1,23 @@
 <?php
     $title = get_field('section_title');
-    $content = get_field('content');
+    $contentFull = get_field('content') ?: '';
+    $contentShort = get_field('content_short') ?: '';
     $button = get_field('button');
     $gallery = get_field( 'gallery');
-
     $block_ID = $block['id'];
     $background_color = get_field('background_color' );
     $image_position = get_field( 'image_position' );
-
     $button_color = $background_color === 'dark-blue' ? 'button-white' : 'button-blue';
     $toggle_content_length_cutting = get_field('toggle_content_length_cutting');
-
     $is_hidden = get_field('is_hidden');
+    $button_text_read_more = get_field('read_more_show') ?: 'Read More';
+    $button_text_read_less = get_field('read_more_hide') ?: 'Read Less';
+    $unique_id = uniqid('block-', true);
 
     if($background_color === 'dark-blue' && str_word_count($title, 0, 'ąćęłńóśźżĄĆĘŁŃÓŚŹŻ') > 1) {
         $title = preg_replace('/\b([\p{L}]+)$/u','<span class="text-highlight">$1</span>', $title);
     }
 
-    $max_chars =  get_field('content_length_for_cut') ?: 400; // Set the maximum number of characters
-    // $truncated_content = mb_substr($content, 0, $max_chars) . (strlen($content) > $max_chars ? '...' : '');
-    $truncated_content = mb_substr($content, 0, $max_chars) . (mb_strlen($content) > $max_chars ? '...' : '');
-
-	// Usuwamy tagi HTML z zawartości pola WYSIWYG
-    $plain_content = wp_strip_all_tags($content);
-    
-    // Tworzymy skrócony tekst, uwzględniając znaki wielobajtowe
-    // $truncated_content = mb_substr($plain_content, 0, $max_chars) . (mb_strlen($plain_content) > $max_chars ? '...' : '');
-
-    // Get the button texts from ACF
-    $button_text_read_more = get_field('read_more_show') ?: 'Read More';
-    $button_text_read_less = get_field('read_more_hide') ?: 'Read Less';
-
-    $unique_id = uniqid('block-', true);
 ?>
 <?php if(!$is_hidden) : ?>
     <section class="block-content-with-gallery block-content-with-gallery--<?= $background_color;?> block-content-with-gallery--<?= $image_position;?>" id="<?= $block_ID; ?>">
@@ -58,29 +44,26 @@
                     <div class="block-content-with-gallery__text fade-in">
  
                     <?php if ($toggle_content_length_cutting): ?>
-                        <!-- Wyświetlamy wersję z ucinaniem i przyciskiem -->
                         <div class="block-content-with-gallery__text-truncated">
-                            <?= $truncated_content; ?>
+                            <?= $contentShort; ?>
                         </div>
+
                         <div class="block-content-with-gallery__text-full" style="display: none;">
-                            <?= $content; ?>
+                            <?= $contentFull; ?>
                         </div>
-                        <?php if (mb_strlen(wp_strip_all_tags($content)) > $max_chars): ?>
-                            <button 
-                                class="block-content-with-gallery__read-more"
-                                data-read-more="<?= esc_attr($button_text_read_more); ?>"
-                                data-read-less="<?= esc_attr($button_text_read_less); ?>"
-                            >
-                                <?= esc_html($button_text_read_more); ?>
-                            </button>
-                        <?php endif; ?>
+
+                        <button 
+                            class="block-content-with-gallery__read-more"
+                            data-read-more="<?= esc_attr($button_text_read_more); ?>"
+                            data-read-less="<?= esc_attr($button_text_read_less); ?>"
+                        >
+                            <?= esc_html($button_text_read_more); ?>
+                        </button>
                     <?php else: ?>
-                        <!-- Wyświetlamy pełny tekst bez przycisku -->
                         <div class="block-content-with-gallery__text-full">
-                            <?= $content; ?>
+                            <?= $contentFull; ?>
                         </div>
                     <?php endif; ?>
-
 
                     </div>
                     <?php if($button) : ?>
